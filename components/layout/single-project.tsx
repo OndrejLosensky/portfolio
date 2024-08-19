d9572d3afba5caf1f723dd735d0dfb90a8f7dcad
Ondřej Losenský <127244546+OndrejLosensky@users.noreply.github.com>
2024-08-19 17:13:36 +0200

    Added coming soon project cards

--- components/layout/single-project.tsx
+++ components/layout/single-project.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import Image from "next/image";
 import { Button } from "../ui/button";
 import Link from "next/link";
@@ -7,8 +5,7 @@ import { CiGlobe } from "react-icons/ci";
 import { FaGithub, FaShoppingBasket } from "react-icons/fa";
 import { projectsData } from "@/lib/projects";
 
-
-type ProjectProps = (typeof projectsData )[number];
+type ProjectProps = (typeof projectsData)[number];
 
 export default function Project({
   title,
@@ -18,59 +15,80 @@ export default function Project({
   githubLink,
   websiteLink,
   otherLink,
-  gradient, 
+  gradient,
+  disabled, 
 }: ProjectProps) {
 
   return (
-    <div className="bg-gray-200 border border-black/30 dark:bg-gray-800 rounded-lg shadow-lg overflow-hidden mx-auto flex flex-col justify-between h-full">
-      <section className="flex flex-col flex-grow">
-        <div className={`w-full bg-gradient-to-br from-green-400 via-blue-500 to-purple-600 dark:from-green-400 dark:via-blue-400 dark:to-purple-500 ${gradient}`}>
+    <div className={`bg-gray-200 border border-black/30 dark:bg-gray-800 rounded-lg shadow-lg overflow-hidden mx-auto flex flex-col justify-between h-full`}>
+      <section className={`flex flex-col flex-grow ${disabled ? 'grayscale' : ''}`}>
+        <div className={`w-full ${disabled ? 'bg-gray-500' : gradient}`}>
           <Image
             src={imageUrl}
-            alt={title} 
+            alt={title}
             quality={90}
             className="w-full p-4 aspect-video h-auto rounded-2xl hover:scale-[1.05] duration-200"
           />
         </div>
         <div className="p-5 flex flex-col flex-grow">
           <div className="flex-grow">
-            <h3 className="dark:text-gray-100 text-gray-700 text-xl font-semibold pt-4 pb-2">{title}</h3>
-            <p className="dark:text-gray-400 text-gray-500 text-sm mb-5">{description}</p>
+            {disabled ? (
+              <h3 className="text-gray-400 text-xl font-semibold pt-4 pb-2 text-center">Brzy...</h3>
+            ) : (
+              <>
+                <h3 className="dark:text-gray-100 text-gray-700 text-xl font-semibold pt-4 pb-2">{title}</h3>
+                <p className="dark:text-gray-400 text-gray-500 text-sm mb-5">{description}</p>
+              </>
+            )}
           </div>
-          <div className="mt-auto">
-            <ul className="flex flex-wrap gap-2 mb-6">
-              {tags.map((tag, index) => (
-                <li key={index} className="dark:bg-gray-700 bg-gray-300 dark:text-white text-black  text-xs rounded-full px-3 py-1">
-                  {tag}
-                </li>
-              ))}
-            </ul>
-            <div className="grid grid-cols-2 xl:grid-cols-3 gap-2">
-              {websiteLink && (
-                <Button variant="ghost" className="bg-gray-400/40 dark:bg-black hover:bg-gray-400/80 dark:hover:bg-black/70 active:scale-90 duration-200">
-                  <Link href={websiteLink} className="flex flex-row items-center space-x-2">
-                    <CiGlobe className="w-5 h-5" /> <span>Web</span>
-                  </Link>
-                </Button>
-              )}
+          {!disabled && (
+            <>
+              <ul className="flex flex-wrap gap-2 mb-6">
+                {tags.map((tag, index) => (
+                  <li
+                    key={index}
+                    className="dark:bg-gray-700 bg-gray-300 dark:text-white text-black text-xs rounded-full px-3 py-1"
+                  >
+                    {tag}
+                  </li>
+                ))}
+              </ul>
+              <div className="grid grid-cols-2 xl:grid-cols-3 gap-2">
+                {websiteLink && (
+                  <Button
+                    variant="ghost"
+                    className="bg-gray-400/40 dark:bg-black hover:bg-gray-400/80 dark:hover:bg-black/70 active:scale-90 duration-200"
+                  >
+                    <Link href={websiteLink} className="flex flex-row items-center space-x-2">
+                      <CiGlobe className="w-5 h-5" /> <span>Web</span>
+                    </Link>
+                  </Button>
+                )}
 
-              {githubLink && (
-                <Button variant="ghost" className="bg-gray-400/40 dark:bg-black hover:bg-gray-400/80 dark:hover:bg-black/70 active:scale-90 duration-200">
-                  <Link href={githubLink} className="flex flex-row items-center space-x-2">
-                    <FaGithub className="w-5 h-5" /> <span>Github</span>
-                  </Link>
-                </Button>
-              )}
+                {githubLink && (
+                  <Button
+                    variant="ghost"
+                    className="bg-gray-400/40 dark:bg-black hover:bg-gray-400/80 dark:hover:bg-black/70 active:scale-90 duration-200"
+                  >
+                    <Link href={githubLink} className="flex flex-row items-center space-x-2">
+                      <FaGithub className="w-5 h-5" /> <span>Github</span>
+                    </Link>
+                  </Button>
+                )}
 
-              {otherLink && (
-                <Button variant="ghost" className="bg-gray-400/40 dark:bg-black hover:bg-gray-400/80 dark:hover:bg-black/70 active:scale-90 duration-200">
-                  <Link href={otherLink} className="flex flex-row items-center gap-x-2">
-                    <FaShoppingBasket className="w-5 h-5" /> <span>Eshop</span>
-                  </Link>
-                </Button>
-              )}
-            </div>
-          </div>
+                {otherLink && (
+                  <Button
+                    variant="ghost"
+                    className="bg-gray-400/40 dark:bg-black hover:bg-gray-400/80 dark:hover:bg-black/70 active:scale-90 duration-200"
+                  >
+                    <Link href={otherLink} className="flex flex-row items-center gap-x-2">
+                      <FaShoppingBasket className="w-5 h-5" /> <span>Eshop</span>
+                    </Link>
+                  </Button>
+                )}
+              </div>
+            </>
+          )}
         </div>
       </section>
     </div>
--- lib/projects.ts
+++ lib/projects.ts
@@ -2,142 +2,73 @@ import datadepot from "@/public/mockups/datadepot.png";
 import kralovskalevandule from "@/public/mockups/kralovska-levandule.png";
 import codecommerce from "@/public/mockups/codecommerce.png";
 import demonicka from "@/public/mockups/demonicka2.png";
+import soon from "@/public/soon.png";
 
 export const projectsData = [
   { 
     title: "Královská levandule",
-    description:
-      "První reálný projekt, který již 2 rokem běží on-line na adrese www.kralovskalevandule.cz.",
+    description: "První reálný projekt, který již 2 rokem běží on-line na adrese www.kralovskalevandule.cz.",
     tags: ["Wordpress", "Figma"],
     imageUrl: kralovskalevandule,
     githubLink: undefined, 
     websiteLink: "https://www.kralovskalevandule.cz",
-    gradient: 'bg-gradient-to-r from-green-400 via-blue-500 to-purple-600 dark:from-green-400 dark:via-blue-500 dark:to-purple-600', 
+    gradient: 'bg-gradient-to-r from-green-400 via-blue-500 to-purple-600 dark:from-green-300 dark:via-teal-500 dark:to-indigo-600', 
     otherLink: "https://obchod.kralovskalevandule.cz",  
+    disabled: false,
   },
   {
     title: "Démonická",
-    description:
-      "Webová aplikace pro akci Démonická pro jednoduchou správu počtu vypitých piv narozdíl proti papírové podobě. ",
+    description: "Webová aplikace pro akci Démonická pro jednoduchou správu počtu vypitých piv narozdíl proti papírové podobě.",
     tags: ["Next.js", "TypeScript", "TailwindCSS", "ClerkAuth", "Firebase"],
     imageUrl: demonicka,
     githubLink: undefined,
     websiteLink: undefined,
-    gradient: 'bg-gradient-to-tr from-green-400 via-blue-500 to-purple-600 dark:from-green-400 dark:via-blue-500 dark:to-purple-600', 
+    gradient: 'bg-gradient-to-r from-green-400 via-blue-500 to-purple-600 dark:from-green-300 dark:via-teal-500 dark:to-indigo-600', 
     otherLink: undefined,
+    disabled: false, 
   }, 
   {
     title: "DataDepot",
-    description:
-      "Jednoduchá aplikace pro správu hesel, poznámek a code snippets vše společně s přehlednou stránkou na statistiky a plně funkční registrací",
+    description: "Jednoduchá aplikace pro správu hesel, poznámek a code snippets vše společně s přehlednou stránkou na statistiky a plně funkční registrací",
     tags: ["Next.js", "JavaScript", "Firebase", "Sqlite", "TailwindCSS"],
     imageUrl: datadepot,
     githubLink: 'https://github.com/OndrejLosensky/DataDepot', 
     websiteLink: undefined,
-    gradient: 'bg-gradient-to-b from-green-400 via-blue-500 to-purple-600 dark:from-green-400 dark:via-blue-500 dark:to-purple-600', 
+    gradient: 'bg-gradient-to-r from-green-400 via-blue-500 to-purple-600 dark:from-green-300 dark:via-teal-500 dark:to-indigo-600', 
     otherLink: undefined,
+    disabled: false,
   },
   {
     title: "CodeCommerce",
-    description:
-      "Webová aplikace pro správu jednoduchého e-shopu pomocí administrátorského panelu",
+    description: "Webová aplikace pro správu jednoduchého e-shopu pomocí administrátorského panelu",
     tags: ["Next.js", "TypeScript", "TailwindCSS", "ClerkAuth", "Prisma"],
     imageUrl: codecommerce,
     githubLink: 'https://github.com/OndrejLosensky/code-commerce',
     websiteLink: undefined,
-    gradient: 'bg-gradient-to-tr from-green-400 via-blue-500 to-purple-600 dark:from-green-400 dark:via-blue-500 dark:to-purple-600', 
+    gradient: 'bg-gradient-to-r from-green-400 via-blue-500 to-purple-600 dark:from-green-300 dark:via-teal-500 dark:to-indigo-600', 
     otherLink: undefined,
+    disabled: false,
   }, 
-] as const;
-
-export const projectsDataAll = [
-  { 
-    title: "Královská levandule",
-    description:
-      "První reálný projekt, který již 2 rokem běží on-line na adrese www.kralovskalevandule.cz.",
-    tags: ["Wordpress", "Figma", "PHP"],
-    imageUrl: kralovskalevandule,
-    githubLink: undefined, 
-    websiteLink: "https://www.kralovskalevandule.cz",
-    gradient: 'from-violet-200 via-sky-300 to-purple-400', 
-    otherLink: "https://obchod.kralovskalevandule.cz",
-  },
-  {
-    title: "Démonická",
-    description:
-      "Webová aplikace pro akci Démonická pro jednoduchou správu počtu vypitých piv narozdíl proti papírové podobě. ",
-    tags: ["Next.js", "TypeScript", "TailwindCSS", "ClerkAuth", "Firebase"],
-    imageUrl: demonicka,
-    githubLink: undefined,
-    websiteLink: undefined,
-    gradient: 'bg-gradient-to-tr from-green-400 via-blue-500 to-purple-600 dark:from-green-400 dark:via-blue-500 dark:to-purple-600', 
-    otherLink: undefined,
-  }, 
-  {
-    title: "LKSYSTEM.CZ s.r.o.",
-    description:
-      "Webová aplikace pro akci Démonická pro jednoduchou správu počtu vypitých piv narozdíl proti papírové podobě. ",
-    tags: ["Next.js", "TypeScript", "TailwindCSS", "ClerkAuth", "Firebase"],
-    imageUrl: demonicka,
-    githubLink: undefined,
-    websiteLink: undefined,
-    gradient: 'bg-gradient-to-tr from-green-400 via-blue-500 to-purple-600 dark:from-green-400 dark:via-blue-500 dark:to-purple-600', 
-    otherLink: undefined,
-  }, 
-  {
-    title: "Vila Perun",
-    description:
-      "Webová aplikace pro akci Démonická pro jednoduchou správu počtu vypitých piv narozdíl proti papírové podobě. ",
-    tags: ["Next.js", "TypeScript", "TailwindCSS", "ClerkAuth", "Firebase"],
-    imageUrl: demonicka,
-    githubLink: undefined,
-    websiteLink: undefined,
-    gradient: 'bg-gradient-to-tr from-green-400 via-blue-500 to-purple-600 dark:from-green-400 dark:via-blue-500 dark:to-purple-600', 
-    otherLink: undefined,
-  },
-  {
-    title: "Pavel Losenský",
-    description:
-      "Webová aplikace pro akci Démonická pro jednoduchou správu počtu vypitých piv narozdíl proti papírové podobě. ",
-    tags: ["Next.js", "TypeScript", "TailwindCSS", "ClerkAuth", "Firebase"],
-    imageUrl: demonicka,
-    githubLink: undefined,
-    websiteLink: undefined,
-    gradient: 'bg-gradient-to-tr from-green-400 via-blue-500 to-purple-600 dark:from-green-400 dark:via-blue-500 dark:to-purple-600', 
-    otherLink: undefined,
-  },  
-  {
-    title: "DataDepot",
-    description:
-      "Jednoduchá aplikace pro správu hesel, poznámek a code snippets vše společně s přehlednou stránkou na statistiky a plně funkční registrací",
-    tags: ["Next.js", "JavaScript", "Firebase", "Sqlite", "TailwindCSS"],
-    imageUrl: datadepot,
-    githubLink: 'https://github.com/OndrejLosensky/DataDepot', 
-    websiteLink: undefined,
-    gradient: 'from-yellow-400 via-red-500 to-pink-500', 
-    otherLink: undefined,
-  },
   {
     title: "CodeCommerce",
-    description:
-      "Webová aplikace pro správu jednoduchého e-shopu pomocí administrátorského panelu",
+    description: "Webová aplikace pro správu jednoduchého e-shopu pomocí administrátorského panelu",
     tags: ["Next.js", "TypeScript", "TailwindCSS", "ClerkAuth", "Prisma"],
-    imageUrl: codecommerce,
+    imageUrl: soon,
     githubLink: 'https://github.com/OndrejLosensky/code-commerce',
     websiteLink: undefined,
-    gradient: 'from-green-400 via-blue-500 to-purple-600', 
+    gradient: 'bg-gradient-to-r from-green-400 via-blue-500 to-purple-600 dark:from-green-300 dark:via-teal-500 dark:to-indigo-600', 
     otherLink: undefined,
+    disabled: true,
   }, 
   {
-    title: "DevDocs",
-    description:
-      "Webová aplikace pro správu jednoduchého e-shopu pomocí administrátorského panelu",
+    title: "CodeCommerce",
+    description: "Webová aplikace pro správu jednoduchého e-shopu pomocí administrátorského panelu",
     tags: ["Next.js", "TypeScript", "TailwindCSS", "ClerkAuth", "Prisma"],
-    imageUrl: codecommerce,
+    imageUrl: soon,
     githubLink: 'https://github.com/OndrejLosensky/code-commerce',
     websiteLink: undefined,
-    gradient: 'from-green-400 via-blue-500 to-purple-600', 
+    gradient: 'bg-gradient-to-r from-green-400 via-blue-500 to-purple-600 dark:from-green-300 dark:via-teal-500 dark:to-indigo-600', 
     otherLink: undefined,
-  }, 
-  
+    disabled: true,
+  },   
 ] as const;
